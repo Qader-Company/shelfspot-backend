1a59b023fc4a20352480dc97ae0675268b2ff326
osama gasser <devosamagasser@gmail.com>
2026-08-13 13:48:25 +0300

    edits new

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Modules\V1\PlatformSettings\Presentation\Http\Controllers\PlatformSettingController;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
 //})->middleware('auth:sanctum');
+
+Route::get('admin/platform-settings/', [PlatformSettingController::class, 'show']);

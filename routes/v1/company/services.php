b36074548541e66137cb093db67c3e8a33f7e40d
osama gasser <devosamagasser@gmail.com>
2026-09-14 13:50:20 +0300

    edits

--- routes/v1/company/services.php
+++ routes/v1/company/services.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Modules\V1\AccessControl\Domain\ValueObjects\CompanyPermissionEnum;
-use App\Modules\V1\Authentication\Domain\ValueObjects\TokenTypeEnum;
 use App\Modules\V1\Services\Presentation\Http\Controller\ServiceController;
-use App\Modules\V1\Users\Domain\ValueObjects\PortalTypeEnum;
-
 
 Route::controller(ServiceController::class)->group(function (){
     Route::get('/', 'index');
